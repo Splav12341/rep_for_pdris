76f836923e96dd7588553d06950296864f9ddb8b
Splav <ziyatdinov.mz@phystech.edu>
2021-11-18 20:52:12 +0300

    added some fixes

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,10 +1,5 @@
 public class MainClass {
 
-
-    public static void main(String[] args) {
-        int a = 10;
-    }
-
     public int intSum(int number1, int number2) {
         return number1 + number2;
     }
